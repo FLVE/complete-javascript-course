e471a99c06af8ba1e6810ef95229ba20518f3acb
jiefangwang <jiefang.wang@outlook.com>
2022-06-29 00:13:18 +0800

    学习到p8节

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN');
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+// 学习到p8
+// https://www.bilibili.com/video/BV1vA4y197C7?p=8&spm_id_from=pageDriver&vd_source=eeff873b457e966f69cb5b174fc0ba3b
