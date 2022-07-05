700e8ef12a7736f5a47d6798f1827ac2419bf2f8
jiefangwang <jiefang.wang@outlook.com>
2022-07-06 01:07:01 +0800

    添加object

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -106,7 +106,7 @@ console.log(yearUntilRetirement(1970, 'Mike'));
 */
 
 // 2、array
-
+/*
 const friend = 'Michael';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
@@ -121,6 +121,100 @@ console.log(friends[0]);
 console.log(friends[2]);
 
 console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob','Alice']
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages)
+
+// 3、array method
+const friends = ['Michael', 'Steven', 'Peter'];
+// 添加元素
+const newsLength = friends.push('Jay');
+console.log(friends);
+console.log(newsLength);
+friends.unshift('John');
+console.log(friends);
+
+// 移除元素
+friends.pop();//Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();//First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+*/
 
 
+// 4、object
+const jonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
 
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 3027 - 1991,
+  job: 'teacher',
+  friedns: ['Micheal', 'Peter', 'Steven']
+};
+
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey])
+console.log(jonas['last' + nameKey])
+
+// console.log(jonas)
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName,lastName,age,job,and friends');
+console.log(jonas[interestedIn]);
+
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log('Wrong request!');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtmann';
+console.log(jonas);
+
+
+console.log(`${jonas.firstName} has ${jonas.friedns.length} friends, and his best friends is called ${jonas.friedns[0]}`);
